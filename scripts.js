ba94cd5649127ced5f62cd3e5ef96eb9ad845a2e
Jong Park <maclay13531@gmail.com>
2017-10-12 18:19:44 -0400

    20171012 version 2

--- scripts.js
+++ scripts.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 			var lon = data.coord.lon;
 			var lat = data.coord.lat;
 			var temp = {
-				current: data.main.temtp,
+				current: data.main.temp,
 				max: data.main.temp_max,
 				min: data.main.temp_min,
 			}
@@ -38,6 +38,7 @@ $(document).ready(function(){
 		var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=e8e54db34d0507b93196869e892e7ae6`;
 
 		$.getJSON(url, function(data){
+			console.log(data);
 			var name = data.name;
 			var lon = data.coord.lon;
 			var lat = data.coord.lat;
@@ -50,6 +51,20 @@ $(document).ready(function(){
 				speed: data.wind.speed,
 				direction: data.wind.deg,
 			}
+			var icon = data.weather[0].icon;
+			var seasonPicture = 0;
+			if(temp.current > 85){
+				seasonPicture = 2;
+			}else if(temp.current > 65){
+				seasonPicture = 3;
+			}else if(temp.curret > 55){
+				seasonPicture = 5;
+			}else{
+				seasonPicture = 4;
+			}
+			$('.image-changer').css({
+				 'background-image': `url('image${seasonPicture}.jpg')`;
+			})
 			$('.city-name-r').text(name);
 			$('.coor-lon-r').text(lon);
 			$('.coor-lat-r').text(lat);
@@ -58,6 +73,11 @@ $(document).ready(function(){
 			$('.temp-min-r').text(temp.min);
 			$('.wind-speed-r').text(wind.speed);
 			$('.wind-direction-r').text(wind.direction);
+			$('.icon-r').html(`<div class="col-sm-12 icon-r"><img src="http://openweathermap.org/img/w/${icon}.png"></div>`);
+			
 		})
 	})
 })
+
+
+
